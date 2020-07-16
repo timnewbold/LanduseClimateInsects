4fb6cd58ae4287f797b4d9c17ee84406f6f6606e
CharlieOuthwaite <charloteouthwaite@gmail.com>
2020-07-16 12:19:38 +0100

    Edits to figure 1 of manuscript

--- 2_RunSimpleLUIModel.R
+++ 2_RunSimpleLUIModel.R
@@ -1,71 +1,145 @@
+##%######################################################%##
+#                                                          #
+####           Land use/use intensity models            ####
+#                                                          #
+##%######################################################%##
+
+# This script runs simple models of biodiversity responses to
+# land use/use intensity only
+
+# load required libraries
 library(StatisticalModels)
+library(ggplot2)
+library(cowplot)
 
+
+# directories
 inDir <- "1_PreparePREDICTSData/"
-
 outDir <- "2_RunSimpleLUIModel/"
 
+# read in the formatted PREDICTS data
 sites <- readRDS(file = paste0(inDir,"PREDICTSSiteData.rds"))
 
+
+## SPECIES RICHNESS MODELS ##
+
+# remove NAs in the specified columns
 model_data <- na.omit(sites[,c('Species_richness','LandUse','Use_intensity','UI2','SS','SSB','SSBS')])
 
+# look at the spread of land use/use intensity categories
 print(table(model_data$UI2))
 
+# run set of simple models with different fixed effects structures
 sm0 <- GLMER(modelData = model_data,responseVar = "Species_richness",fitFamily = "poisson",
              fixedStruct = "1",randomStruct = "(1|SS)+(1|SSB)+(1|SSBS)",REML = FALSE)
+
 sm1 <- GLMER(modelData = model_data,responseVar = "Species_richness",fitFamily = "poisson",
              fixedStruct = "LandUse",randomStruct = "(1|SS)+(1|SSB)+(1|SSBS)",REML = FALSE)
+
 sm2 <- GLMER(modelData = model_data,responseVar = "Species_richness",fitFamily = "poisson",
              fixedStruct = "LandUse+Use_intensity",randomStruct = "(1|SS)+(1|SSB)+(1|SSBS)",REML = FALSE)
+
 sm3 <- GLMER(modelData = model_data,responseVar = "Species_richness",fitFamily = "poisson",
              fixedStruct = "UI2",randomStruct = "(1|SS)+(1|SSB)+(1|SSBS)",REML = FALSE)
+
 sm4 <- GLMER(modelData = model_data,responseVar = "Species_richness",fitFamily = "poisson",
              fixedStruct = "LandUse*Use_intensity",randomStruct = "(1|SS)+(1|SSB)+(1|SSBS)",REML = FALSE)
+# fixed-effect model matrix is rank deficient so dropping 3 columns / coefficients
 
+# take a look at the AICs
 print(AIC(sm0$model,sm1$model,sm2$model,sm3$model,sm4$model))
+
+
+## ABUNDANCE MODELS ##
 
 model_data <- na.omit(sites[,c('LogAbund','LandUse','Use_intensity','UI2','SS','SSB','SSBS')])
 
 am0 <- GLMER(modelData = model_data,responseVar = "LogAbund",fitFamily = "gaussian",
              fixedStruct = "1",randomStruct = "(1|SS)+(1|SSB)",REML = FALSE)
+
 am1 <- GLMER(modelData = model_data,responseVar = "LogAbund",fitFamily = "gaussian",
              fixedStruct = "LandUse",randomStruct = "(1|SS)+(1|SSB)",REML = FALSE)
+
 am2 <- GLMER(modelData = model_data,responseVar = "LogAbund",fitFamily = "gaussian",
              fixedStruct = "LandUse+Use_intensity",randomStruct = "(1|SS)+(1|SSB)",REML = FALSE)
+
 am3 <- GLMER(modelData = model_data,responseVar = "LogAbund",fitFamily = "gaussian",
              fixedStruct = "UI2",randomStruct = "(1|SS)+(1|SSB)",REML = FALSE)
+
 am4 <- GLMER(modelData = model_data,responseVar = "LogAbund",fitFamily = "gaussian",
              fixedStruct = "LandUse*Use_intensity",randomStruct = "(1|SS)+(1|SSB)",REML = FALSE)
+#fixed-effect model matrix is rank deficient so dropping 3 columns / coefficients
 
+
+# take a look at the AICs
 print(AIC(am0$model,am1$model,am2$model,am3$model,am4$model))
+
+
+### Predict responses for plotting ###
+
 
 nd <- data.frame(UI2=factor(c("Primary vegetation","Secondary vegetation",
                               "Agriculture_Low","Agriculture_High","Urban")),
                  Species_richness=0,
                  LogAbund=0)
 
-s.preds <- PredictGLMERRandIter(model = sm3$model,data = nd)
+## species richness predictions ##
+
+s.preds <- PredictGLMERRandIter(model = sm3$model, data = nd)
 
 s.preds <- exp(s.preds)
 
-s.preds <- sweep(x = s.preds,MARGIN = 2,STATS = s.preds[1,],FUN = '/')
+# convert to percentage difference from primary vegetation
+s.preds <- sweep(x = s.preds, MARGIN = 2, STATS = s.preds[1,], FUN = '/')
 
+# get quantiles
 s.preds.median <- ((apply(X = s.preds,MARGIN = 1,FUN = median))*100)-100
 s.preds.upper <- ((apply(X = s.preds,MARGIN = 1,FUN = quantile,probs = 0.975))*100)-100
 s.preds.lower <- ((apply(X = s.preds,MARGIN = 1,FUN = quantile,probs = 0.025))*100)-100
+
+
+## abundance predictions ##
 
 a.preds <- PredictGLMERRandIter(model = am3$model,data = nd)
 
 a.preds <- exp(a.preds)-1
 
+# convert to percentage difference from primary vegetation
 a.preds <- sweep(x = a.preds,MARGIN = 2,STATS = a.preds[1,],FUN = '/')
 
+# get quantiles
 a.preds.median <- ((apply(X = a.preds,MARGIN = 1,FUN = median))*100)-100
 a.preds.upper <- ((apply(X = a.preds,MARGIN = 1,FUN = quantile,probs = 0.975))*100)-100
 a.preds.lower <- ((apply(X = a.preds,MARGIN = 1,FUN = quantile,probs = 0.025))*100)-100
 
-pdf(file = paste0(outDir,"LUI_Plot.pdf"),width = 8.5/2.54,height = 12/2.54)
 
-par(mfrow=c(2,1))
+
+### plot species richness and abundance predictions ###
+
+# Figure 1, includes map of sites:
+
+# load world map
+map.world <- map_data('world')
+
+# map of sites
+p1 <-ggplot() +
+  geom_map(data=map.world, map=map.world,
+           aes(x=long, y=lat, group=group, map_id=region),
+           fill= "grey", colour="grey", size=0.2) +
+  geom_point(data = sites, aes(x = Longitude, y = Latitude), col = c("#1E90FF"), fill = c("#104E8B"), shape = 21) +
+  theme(axis.title = element_blank(), 
+        plot.background = element_blank(), 
+        panel.background = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        plot.title = element_text(hjust=0.05)) +
+  ggtitle("a.")
+
+
+#pdf(file = paste0(outDir,"LUI_Plot.pdf"),width = 8.5/2.54,height = 12/2.54, onefile = T)
+
+
+#par(mfrow=c(2,1))
 par(cex=1)
 par(cex.lab=1)
 par(cex.axis=1)
@@ -73,11 +147,13 @@ par(cex.main=1)
 par(ps=10)
 par(las=1)
 par(mgp=c(1.6,0.2,0))
-par(mar=c(2.6,2.6,0.5,0.5))
+par(mar=c(2.6,2.6,1.5,1))
 par(tck=-0.01)
 
-
+# set colours
 errbar.cols <- c("#009E73","#0072B2","#E69F00","#D55E00","#CC79A7")
+
+# species richness plot
 
 errbar(x = 1:5,y = s.preds.median,yplus = s.preds.upper,yminus = s.preds.lower,
        col=errbar.cols,errbar.col = errbar.cols,ylim=c(min(s.preds.lower),max(s.preds.upper)),xaxt="n",
@@ -87,6 +163,12 @@ axis(side = 1,at = 1:5,labels = c("PV","SV","AG.Low","AG.Hi","URB"))
 
 abline(h=0,col="#00000077",lty=2)
 
+title(main = "b.", adj = 0, cex.main = 1, line = 1)
+
+p2 <- recordPlot()
+
+# abundance plot
+
 errbar(x = 1:5,y = a.preds.median,yplus = a.preds.upper,yminus = a.preds.lower,
        col=errbar.cols,errbar.col = errbar.cols,ylim=c(min(a.preds.lower),max(a.preds.upper)),xaxt="n",
        ylab="Total abundance (%)",xlab="Land use",bty="l")
@@ -95,4 +177,17 @@ axis(side = 1,at = 1:5,labels = c("PV","SV","AG.Low","AG.Hi","URB"))
 
 abline(h=0,col="#00000077",lty=2)
 
-invisible(dev.off())
+title(main = "c.", adj = 0, cex.main = 1, line = 1)
+
+p3 <- recordPlot()
+
+
+#invisible(dev.off())
+
+# Organise the plots
+
+p4 <- plot_grid(p1, p2, p3, ncol = 1, scale = 0.85)
+
+# save figure
+save_plot(paste0(outDir, "Figure_1.pdf"), p4, base_height = 8, base_width = 6)
+
